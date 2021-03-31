23b93718a4399accf3e9d4b25cc4ac921f191d15
Miles Lucas <mdlucas@hawaii.edu>
2021-03-31 16:50:02 -0500

    add more integration tests

--- test/conversions.jl
+++ test/conversions.jl
@@ -19,14 +19,14 @@ function test_integration(f1, f2, angles)
 end
 
 @testset "integration - $f1/$f2" for (f1, f2) in zip(
-    [deg2hms, deg2dms], [hms2deg, dms2deg]
+    [deg2hms, deg2dms, deg2ha], [hms2deg, dms2deg, ha2deg]
 )
     angles = randdegree(rng, 1000)
     test_integration(f1, f2, angles)
 end
 
 @testset "integration - $f1/$f2" for (f1, f2) in zip(
-    [rad2hms, rad2dms], [hms2rad, dms2rad]
+    [rad2hms, rad2dms, rad2ha], [hms2rad, dms2rad, ha2rad]
 )
     angles = randrad(rng, 1000)
     test_integration(f1, f2, angles)
